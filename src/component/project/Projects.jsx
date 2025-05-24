563d3b957d0b852f79e5018545176cca34b50004
shofiq18 <shafiqul.islam25021998@gmail.com>
2025-05-25 00:39:09 +0600

    added some new projects and skills also update resume file

--- src/component/project/Projects.jsx
+++ src/component/project/Projects.jsx
@@ -2,21 +2,47 @@ import React, { useState } from 'react';
 import ProjectModal from './ProjectModal';
 
 const projectsData = [
-  {
-    name: 'Adventure Mountain',
-    image: 'https://i.ibb.co/1JvmLjY/adventure-mountain.jpg',
-    description: 'An online platform for adventure enthusiasts.',
-    stack: ['React', 'Node.js', 'MongoDB', 'Tailwind CSS'],
-    liveLink: 'https://adventure-mountain-6182e.web.app/',
-    githubLink: 'https://github.com/programming-hero-web-course1/b10-a9-authentication-shofiq18',
-    details: 'This project offers a comprehensive guide to mountain adventures, including trail information, gear recommendations, and community forums.',
-    challenges: 'Implementing real-time updates for trail conditions and integrating user-generated content posed significant challenges.',
-    improvements: 'Plans to enhance mobile responsiveness and add a recommendation system based on user preferences.',
+    {
+    name: 'TestyRide -Food Delivery website',
+    image: 'https://i.ibb.co/sdWkpgLf/Screenshot-4.png',
+    description: 'A food delivery website where users can order food from their favorite restaurants.',
+    stack: ['React', 'Node.js', 'MongoDB', 'Tailwind CSS', 'Express', 'Firebase', 'moongoose', 'tan stack'],
+    liveLink: 'https://tastyride-cd1a3.web.app/',
+    githubLink: 'https://github.com/safin33221/TastyRide-Client-Side',
+    details: ' This website for food delivery allows users to order food from various restaurants, track their orders, and manage their profiles.',
+    challenges: 'Managing large media files and ensuring a smooth user experience across different devices were significant challenges.',
+    improvements: 'adding rider chat feature and integrating a shopping list generator.',
   },
+    
+    {
+    name: 'Piece Work - Micro Job Platform',
+    image: 'https://i.ibb.co/4g50CwZr/Screenshot-8.png',
+    description: 'A micro job platform connecting freelancers with clients for small tasks.',
+    stack: ['React', 'Node.js', 'MongoDB', 'Tailwind CSS', 'stripe', 'Express', 'Firebase'],
+    liveLink: 'https://piece-work-e8024.web.app/',
+    githubLink: 'https://github.com/shofiq18/piece-work-client',
+    details: 'This platform connects freelancers with clients for small tasks, allowing users to post jobs, apply for them, and manage their profiles.',
+    challenges: 'Ensuring secure transactions and managing user data privacy were significant challenges.',
+    improvements: 'Plans to add a review system for freelancers and clients, and enhance the user interface.',
+  },
+  
+    {
+    name: 'Food Sharing Website',
+    image: 'https://i.ibb.co/Z1d27ZQL/Screenshot-7.png',
+    description: 'A community-driven site for sharing recipes and cooking tips.',
+    stack: ['React', 'Node.js', 'MongoDB', 'Tailwind CSS'],
+    liveLink: 'https://food-bd-31846.web.app/',
+    githubLink: 'https://github.com/programming-hero-web-course2/b10a11-client-side-shofiq18',
+    details: 'This website enables users to share recipes, cooking tips, and meal plans, fostering a community of food enthusiasts.',
+    challenges: 'Managing large media files and ensuring a smooth user experience across different devices were significant challenges.',
+    improvements: 'Plans to add a meal planning feature and integrate a shopping list generator.',
+  },
+  
+
   {
     name: 'Movie Portal',
     image: 'https://i.ibb.co/bHTLgBp/movie-portal.jpg',
-    description: 'A platform to discover and watch movies and TV shows.',
+    description: 'A platform to discover and watch movies and TV shows where users can create watchlists and get recommendations.',
     stack: ['React', 'Node.js', 'MongoDB', 'Tailwind CSS'],
     liveLink: 'https://movie-portal-4ef85.web.app/',
     githubLink: 'https://github.com/programming-hero-web-course2/b10-a10-client-side-shofiq18',
@@ -25,16 +51,27 @@ const projectsData = [
     improvements: 'Plans to implement a user rating system and enhance the recommendation algorithm.',
   },
   {
-    name: 'Food Sharing Website',
-    image: 'https://i.ibb.co/H7tBgkf/foodbridge.jpg',
-    description: 'A community-driven site for sharing recipes and cooking tips.',
+    name: 'Adventure Mountain',
+    image: 'https://i.ibb.co/1JvmLjY/adventure-mountain.jpg',
+    description: 'An online platform for adventure enthusiasts and outdoor lovers where they can find and share information about hiking trails, gear recommendations, and outdoor events.',
     stack: ['React', 'Node.js', 'MongoDB', 'Tailwind CSS'],
-    liveLink: 'https://food-bd-31846.web.app/login',
-    githubLink: 'https://github.com/programming-hero-web-course2/b10a11-client-side-shofiq18',
-    details: 'This website enables users to share recipes, cooking tips, and meal plans, fostering a community of food enthusiasts.',
-    challenges: 'Managing large media files and ensuring a smooth user experience across different devices were significant challenges.',
-    improvements: 'Plans to add a meal planning feature and integrate a shopping list generator.',
-  }
+    liveLink: 'https://adventure-mountain-6182e.web.app/',
+    githubLink: 'https://github.com/programming-hero-web-course1/b10-a9-authentication-shofiq18',
+    details: 'This project offers a comprehensive guide to mountain adventures, including trail information, gear recommendations, and community forums.',
+    challenges: 'Implementing real-time updates for trail conditions and integrating user-generated content posed significant challenges.',
+    improvements: 'Plans to enhance mobile responsiveness and add a recommendation system based on user preferences.',
+  }, 
+   {
+    name: 'BPL Dream 11 - Cricket Fantasy League',
+    image: 'https://i.ibb.co/HTP6WBzd/Screenshot-6.png',
+    description: 'A fantasy cricket league platform for fans to create and manage their teams where they can join leagues and compete with others.',
+    stack: ['React', 'Tailwind CSS', 'firebase', ],
+    liveLink: 'https://splendid-queen.surge.sh',
+    githubLink: 'https://github.com/shofiq18/bpl-dream-11',
+    details: 'This website enables users to create their fantasy cricket teams, join leagues, and compete with others.',
+    challenges: 'Implementing real-time scoring and ensuring fair play were significant challenges.',
+    improvements: 'Plans to add more player statistics and enhance the user interface.',
+  },
   // Add more projects here
 ];
 
